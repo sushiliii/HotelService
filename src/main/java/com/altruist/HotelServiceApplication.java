aabfca92a4887cc101d92847c940bef9cd928b23
Sushil <sushil.mehta@altruistindia.com>
2024-09-18 14:28:55 +0530

    commited in main file 1

--- src/main/java/com/altruist/HotelServiceApplication.java
+++ src/main/java/com/altruist/HotelServiceApplication.java
@@ -8,7 +8,7 @@ public class HotelServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelServiceApplication.class, args);
-		System.out.println("Started");i
+		System.out.println("Started");
 	}
 
 }
